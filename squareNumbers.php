8db14089221bab47d42cb2609d01e5ff87683795
richmartins <richard.martinstenorio@gmail.com>
2019-12-20 16:39:45 +0100

    [DONE] php version before X-MAS :DDDD

--- squareNumbers.php
+++ squareNumbers.php
@@ -3,7 +3,7 @@
 $size = $argv[1];
 
 function row($size){
-  print " row : $size * $size\n";
+  print " row: $size * $size\n";
   $total = $size * $size;
   $current = $size;
   for ($i=1; $i <= $total; $i++) {
@@ -16,7 +16,7 @@ function row($size){
 }
 
 function column($size){
-  print " column : $size * $size\n";
+  print " column: $size * $size\n";
   $total = $size * $size;
   $counter = $size;
   for ($i=1; $i <= $size ; $i++) {
@@ -29,30 +29,75 @@ function column($size){
     print"\n";
   }
 }
-// to do
-// function spiral($size){
-//   $total = $size * $size;
-//   $numbers = [];
-//   for ($i=0; $i <= $total; $i++) {
-//     $numbers[$i] = $i;
-//     for ($j=0; $j <= $size; $j++) {
-//       $numbers[$j] = $j;
-//     }
-//   }
-//
-//   foreach ($numbers as $k => $v) {
-//     if($v === 0){
-//       continue;
-//     }
-//     print $v;
-//     if ($k % $size == 0){
-//       print "\n";
-//     }
-//   }
-// }
+
+function spiral($size){
+  $square = $size * $size;
+  $array = [];
+  for($i = 0; $i < $size; $i++) {
+    $array[$i] = [];
+    for($j = 0; $j < $size; $j++) {
+      $array[$i][$j] = 0;
+    }
+  }
+
+  $x = $size -1;
+  $y = 0;
+
+  $dx = -1;
+  $dy = 0;
+
+  for($i = $square; $i > 0; $i--) {
+    $array[$y][$x] = "$i $x $y";
+    
+    $new_x = $x + $dx;
+    $new_y = $y + $dy;
+
+    if ($new_x >= 0 && $new_x < $size && $new_y >= 0 && $new_y < $size && !($array[$new_y][$new_x])){
+      $x = $new_x;
+      $y = $new_y;
+    } else {
+      $res = turn($dx, $dy);
+      $dx = $res[0];
+      $dy = $res[1];
+      
+      $x = $x + $dx;
+      $y = $y + $dy;
+    }
+  }
+
+  // print_r($array);
+  echo (" spiral: $size * $size\n");
+  foreach($array as $line) {
+    foreach($line as $v) {
+      printf(" %02d", $v);
+    }
+    printf("\n");
+  }
+}
+
+function turn($dx, $dy){
+  if ($dx === 0 && $dy === -1) {            // UP
+    $dx = -1;                               // LEFT
+    $dy = 0;
+  } elseif ($dx === 1 && $dy === 0) {       // RIGHT
+    $dx = 0;                                // UP
+    $dy = -1;
+  } elseif ($dx === 0 && $dy === 1) {       // DOWN
+    $dx = 1;                                // RIGHT
+    $dy = 0;
+  } elseif($dx === -1 && $dy === 0) {       // LEFT
+    $dx = 0;                                // DOWN
+    $dy = 1;
+  } else {
+    die('yolo');
+  }
+
+  return [$dx, $dy];
+}
+
 
 row($size);
 print "\n\n";
 column($size);
 print "\n\n";
-//spiral($size);
+spiral($size);
