0fc6d0ac5f3a733bd9924edcbaccd44ea7fc3e2e
richmartins <richard.martinstenorio@gmail.com>
2020-11-02 00:38:37 +0100

    cpp version done

--- squareNumbers.cpp
+++ squareNumbers.cpp
@@ -3,7 +3,7 @@
  * author      : Richard Tenorio
  * date        : 2020-10-15
  * description : Read the README.md file
- * compiler    : g++ Apple clang version 12.0.0 (clang-1200.0.32.2)
+ * compiler    : gcc version 10.2.0 (Homebrew GCC 10.2.0)
  * flags       : -DCMAKE_CXX_FLAGS="-Wall -Wextra -Wconversion
  *               -Wsign-conversion -pedantic"
  * -----------------------------------------------------------------
@@ -16,24 +16,35 @@
 
 using namespace std;
 
-int row(int n, int nbDigits);    // done
-int column(int n, int nbDigits); // done
-int spiral(int n, int nbDigits); // WIP
-int * turn(int dx, int dy);
+int row(int n, int nbDigits);
+int column(int n, int nbDigits);
+int spiral(int n, int nbDigits);
+void turn(int& dx, int& dy);
 
 int main(int argc, char** argv) {
-    if (argc >= 1 && stoi(argv[1])) {
-        int n = stoi(argv[1]);
-        int nbDigits = (int)log10((n*n))+1; //get number of digits of square n
+
+    if (argc >= 1 && argv[1] != NULL){
+        int n;
+        try {
+            n = stoi(argv[1]);
+        } catch(invalid_argument& e){
+            cout << "you must pass an integer as argument";
+            return EXIT_FAILURE;
+        }catch(out_of_range& e){
+            cout << "the integer passed was too long";
+            return EXIT_FAILURE;
+        }
+
+        int nbDigits = (int)log10((n*n))+1; //get number of digits n squared
 
         row(n,nbDigits);
         column(n, nbDigits);
         spiral(n, nbDigits);
 
-    } else {
-        cout << "You must pass an argument and it should be an integer"
-             << endl;
-    }
+   } else {
+       cout << "You must pass an integer as argument to run this program"
+            << endl;
+   }
 
     return EXIT_SUCCESS;
 }
@@ -64,7 +75,7 @@ int column(int n, int nbDigits){
     int counter = n;
 
     for(int i = 1; i <= n; ++i){
-        cout << setw(2) << setfill('0') << i << " ";
+        cout << setw(nbDigits) << setfill('0') << i << " ";
         int curr = i;
         for(int j = 1; j < counter; ++j){
             curr += n;
@@ -74,12 +85,8 @@ int column(int n, int nbDigits){
     }
 
     cout << endl;
-    return 0;
+    return EXIT_SUCCESS;
 }
-
-/*
- * TODO: make it work lol
- */
 
 int spiral(int n, int nbDigits){
     int square = n * n;
@@ -98,64 +105,61 @@ int spiral(int n, int nbDigits){
     int y = 0;
 
     int dx = -1;
-    int dy = -1;
+    int dy = 0;
 
-    for(int i = square; i > 0; --i){
+    int new_x;
+    int new_y;
 
-        int new_x = x + dx;
-        int new_y = y + dy;
 
-        if(new_x >= 0 && new_x < n && new_y >= 0 && new_y < n && ! (matrice[new_y][new_x])){
+    for(int current = square; current > 0; --current){
+        matrice[y][x] = current;
+
+        new_x = x + dx;
+        new_y = y + dy;
+
+        if(new_x >= 0
+           && new_x < n
+           && new_y >= 0
+           && new_y < n
+           && !matrice[new_y][new_x]){
+
             x = new_x;
             y = new_y;
-        } else {
-            int *res = turn(dx, dy);
-            dx = res[0];
-            dy = res[1];
 
+        } else {
+            turn(dx, dy);
             x = x + dx;
             y = y + dy;
         }
-        // for(int line : matrice[]){
-            // for(int value : line){
-
-            // }
-        // }
     }
-
 
     // print matrice
     for(int i = 0; i < n; ++i){
-        for (int j = 0; j < n; ++j){
-            cout << setw(2) << setfill('0') << matrice[i][j] << " ";
+        for(int j = 0; j < n; ++j){
+            cout << setw(nbDigits) << setfill('0') << matrice[i][j] << " ";
         }
         cout << endl;
     }
-
     cout << endl;
-    return 0;
+    return EXIT_SUCCESS;
 }
 
-int * turn(int dx, int dy){
-    if(dx == 0 && dy == -1){
-        dx = -1;
+void turn(int& dx, int& dy){
+    if (dx == 0 && dy == -1) {            // UP
+        dx = -1;                               // LEFT
         dy = 0;
-    } else if (dx == 1 && dy == 0){
-        dx = 0;
+    } else if (dx == 1 && dy == 0) {       // RIGHT
+        dx = 0;                                // UP
         dy = -1;
-    } else if (dx == 0 && dy == 1){
-        dx = 1;
+    } else if (dx == 0 && dy == 1) {       // DOWN
+        dx = 1;                                // RIGHT
         dy = 0;
-    } else if (dx == -1 && dy == 0){
-        dx = 0;
+    } else if(dx == -1 && dy == 0) {       // LEFT
+        dx = 0;                                // DOWN
         dy = 1;
     } else {
-        cout << "prout" <<  endl;
+        cout << "prout" << endl;
     }
 
-    int res[2];
-    res[0] = dx;
-    res[1] = dy;
-
-    return res;
 }
+
