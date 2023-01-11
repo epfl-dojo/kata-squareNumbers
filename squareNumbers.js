67597617272db4bc8fcb5922b9fc74b4f3bded51
greg <gd.temp@pm.me>
2023-01-11 11:50:00 +0100

    Some refactor of the inline dsplay

--- squareNumbers.js
+++ squareNumbers.js
@@ -2,7 +2,7 @@ const prompt = require("prompt-sync")({ sigint: true })
 
 let squareSize = prompt("What size do you want?")
 
-let count = 1
+let count1 = 1
 let squareArray1 = []
 let output1 = ""
 
@@ -10,10 +10,10 @@ let output1 = ""
 for (let i = 0; i < squareSize; i++) {
     squareArray1[i] = []
     for (let j = 0; j <= squareSize - 1; j++) {
-        squareArray1[i][j] = count
-        count++
+        squareArray1[i][j] = count1
+        count1++
     }
-    output1 += squareArray1[i] + "\n";
+    output1 += squareArray1[i] + "\n"
 }
 console.log(output1);
 
@@ -21,11 +21,14 @@ console.log(output1);
 //in column
 let squareArray2 = []
 let output2 = ""
+let count2 = 1
 for (let i = 0; i < squareSize; i++) {
-    squareArray2[i] = []
-    squareArray2[i] = count
-    count++
-    j++
+    //squareArray2[i] = []
+    for (let j = 0; j < squareSize; j++) {
+        squareArray2[j][i] = count2
+        count2++
+    }
+    output2 += squareArray2[i] + "\n"
 }
 console.log(squareArray2)
 
